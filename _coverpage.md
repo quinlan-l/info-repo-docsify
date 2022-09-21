6cb2077d89b7d7822915ddc0a5fa8d3c5b6eca7f
quinlan-l <quinlan_l@qq.com>
2022-09-20 22:32:42 -0700

    gen-sidebar setting

--- _coverpage.md
+++ _coverpage.md
@@ -1,11 +1,11 @@
-![logo](static/icon.svg)
+![logo](_static/icon.svg)
 
 # 一闪亮晶 <small>v1.0</small>
 
 > 基于Markdown文档管理与维护的个人知识库
 
 <audio id="player" controls autoplay>
-    <source src="static/Mozart.K.265-K.300e.Theme.mp3" type="audio/mp3">
+    <source src="_static/Mozart.K.265-K.300e.Theme.mp3" type="audio/mp3">
 </audio>
 
 [GitHub](https://github.com/quinlan-l/info-repo)
--- gen-sidebar.sh
+++ gen-sidebar.sh
@@ -4,12 +4,12 @@ echo '' > _sidebar.md
 
 find . -path ./.git -prune -o -type d -o -type f -name "*.md" | 
 awk -F '[/]' 'NF>1{
-    if(match($NF, "_sidebar") || match($NF, "README") || match($NF, ".git")){
+    if(substr($NF, 1, 1)=="_" || match($NF, "README") || substr($NF, 1, 1)=="."){
         # 不作处理
     }else{
         for(i=2;i<NF;i++){
             # 按目录层级设置缩进格式
-            printf "    " >> "_sidebar.md"
+            printf "  " >> "_sidebar.md"
         }
         if (match($NF, ".md")){
             # 如果是md文件，则不需要尾部的“.md”和头部"./"
